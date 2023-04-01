28d285bd12f7bfaf1ee1d27d4231d20b2e9d58eb
brightblueray <rryjewski@hashicorp.com>
2023-04-01 07:20:52 -0400

    Updated Names of HCP Resources

--- 01_HCP_Config/modules/hcp_demo/main.tf
+++ 01_HCP_Config/modules/hcp_demo/main.tf
@@ -27,7 +27,7 @@ variable "vault_cluster_id" {}
 variable "vault_cluster_tier" {}
 
 # Create Vault Cluster -  A Small Demo Cluster ($0.03/hr or about $20/month)
-resource "hcp_vault_cluster" "demo_cluster" {
+resource "hcp_vault_cluster" "demo-vault" {
   cluster_id        = var.vault_cluster_id
   hvn_id            = hcp_hvn.hvn.hvn_id
   tier              = var.vault_cluster_tier
@@ -36,18 +36,18 @@ resource "hcp_vault_cluster" "demo_cluster" {
 }
 
 # Get an Admin Token
-resource "hcp_vault_cluster_admin_token" "demo" {
-  cluster_id = hcp_vault_cluster.demo_cluster.cluster_id
+resource "hcp_vault_cluster_admin_token" "demo-token" {
+  cluster_id = hcp_vault_cluster.demo-vault.cluster_id
 }
 
 # Outputs
 output "vault_addr" {
   description = "Vault Instance Address"
-  value       = hcp_vault_cluster.demo_cluster.vault_public_endpoint_url
+  value       = hcp_vault_cluster.demo-vault.vault_public_endpoint_url
 }
 
 output "vault_token" {
-  value = hcp_vault_cluster_admin_token.demo.token
+  value = hcp_vault_cluster_admin_token.demo-token.token
   sensitive = true
 }
 
@@ -62,7 +62,7 @@ variable "boundary_user" {}
 variable "boundary_password" {sensitive = true}
 
 # Create Boundary Cluster
-resource "hcp_boundary_cluster" "demo_cluster" {
+resource "hcp_boundary_cluster" "demo-boundary" {
   cluster_id = var.boundary_cluster_id
   username   = var.boundary_user
   password   = var.boundary_password
@@ -71,19 +71,16 @@ resource "hcp_boundary_cluster" "demo_cluster" {
 # Outputs
 output "boundary_addr" {
   description = "Boundary Instance Address"
-  value       = hcp_boundary_cluster.demo_cluster.cluster_url
+  value       = hcp_boundary_cluster.demo-boundary.cluster_url
 }
 
 output "boundary_user" {
-  description = "Boundary user "
+  description = "Boundary Admin user "
   value       = var.boundary_user
 }
 
 output "boundary_password" {
+  description = "Boundary Admin User"
   value = var.boundary_password
   sensitive = true
 }
-
-
-
-
